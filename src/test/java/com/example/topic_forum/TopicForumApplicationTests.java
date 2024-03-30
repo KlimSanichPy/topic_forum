939cc92992ee81d734fe5b090234fd0ec0f1824c
Макаревич Матвей <makarevich.matv@mail.ru>
2024-03-30 18:37:19 +0300

    Initial commit

---
+++ src/test/java/com/example/topic_forum/TopicForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topic_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
